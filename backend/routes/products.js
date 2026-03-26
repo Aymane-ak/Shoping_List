186d3bc982cca63c69c9d74a9e78f8be7db3c972
aymane <aymaneak30@gmail.com>
2026-03-26 01:59:44 +0100

    feat: add product search route with Open Food Facts API

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,9 +3,61 @@ const pool    = require('../db');
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', async (req, res) => {
-    const result = await pool.query( 'SELECT * FROM products')
+router.get('/:idList', async (req, res) => {
+    const result = await pool.query( 'SELECT * FROM products where list_id = $1', [req.params.id])
     res.json(result.rows)
 })
+
+router.post('/lists/:idList/product',async (req,res) => {
+
+
+    if(! req.params.idList){
+        return res.status(400).json({ error : "Id non fourni"} )
+    }
+    try {
+        const result = await pool.query('INSERT INTO products (name,description,price,list_id) VALUES ($1,$2,$3,$4)', [req.body.name,
+        req.body.description,req.body.price,req.params.idList])
+        res.json(result.rows);
+
+    }
+    catch(error) {
+
+        res.status(500).json( {error : error.message} )
+    }
+})
+
+
+/* 
+    APPEL API EXTERNE 
+*/
+
+router.get("/",async(req,res)=> {    
+
+    if ( !req.query.name) {
+
+        return res.status(400).json( {error  : " No product name"} )
+    }
+    try {
+        const response = await fetch (`https://world.openfoodfacts.net/api/v2/search?search_terms=${req.query.name}&json=true&page_size=10`)
+        const data     = await response.json()        
+        const dattaSimplified = data.products.map( product =>(  {
+            name         : product.product_name,
+            image_url    : product.image_front_url,
+            barcode      : product.id,
+            brand        : product.brands,
+            calories     : product.nutriments?.["energy-kcal"],
+            product_size : product.product_quantity,
+            nutriscore   : product.nutrition_grade_fr
+        }))
+        console.log("Résultat de la requête :", dattaSimplified)
+        res.json(dattaSimplified)
+        
+    }
+    catch ( error ){
+        return res.status(500).json({error : error.message })
+    }
+    
+})
+
 
 module.exports = router
--- database.sql
+++ database.sql
@@ -8,9 +8,15 @@ CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50),
   description VARCHAR(150),
+  image_url TEXT, 
   price DECIMAL,
-  bought BOOLEAN,
+  barcode VARCHAR(50),
+  brand VARCHAR(100),
+  calories DECIMAL,
+  product_size VARCHAR(50)
+  bought BOOLEAN  DEFAULT false ,
   created_at TIMESTAMP DEFAULT NOW(), 
-  list_id INT REFERENCES lists(id)
+  list_id INT REFERENCES lists(id),
+  nutriscore VARCHAR(5)
 );
 
